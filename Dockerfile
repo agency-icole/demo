6907b0673401b543d9e194fbe8783258ff30bee3
aradosz79 <artur@icole.eu>
2019-08-26 01:37:16 +0200

    removed dependency; changed cmd

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM agencyicole/runtime:1.0.0
 COPY target/classes /app
 COPY target/lib /app/lib
 
-CMD ["/bin/bash","cd /app; java -cp .:lib\*.jar com.example.demo.DemoApplication"]
+CMD cd /app; java -cp .:lib\*.jar com.example.demo.DemoApplication
 
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,8 @@
 package com.example.demo;
 
 import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.statsd.StatsdConfig;
-import io.micrometer.statsd.StatsdFlavor;
-import io.micrometer.statsd.StatsdMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -25,20 +19,6 @@ public class DemoApplication {
     }
 
     public static void main(String[] args) {
-
-        StatsdConfig config = new StatsdConfig() {
-            @Override
-            public String get(String k) {
-                return null;
-            }
-
-            @Override
-            public StatsdFlavor flavor() {
-                return StatsdFlavor.TELEGRAF;
-            }
-        };
-
-        //MeterRegistry registry = new StatsdMeterRegistry(config, Clock.SYSTEM);
         SpringApplication.run(DemoApplication.class, args);
     }
 
