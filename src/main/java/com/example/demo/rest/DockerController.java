40417974f472826784fe6f8873dfe61f83eb5cef
aradosz79 <artur@icole.eu>
2019-08-31 01:38:59 +0200

    added unhealthy status

--- src/main/java/com/example/demo/rest/DockerController.java
+++ src/main/java/com/example/demo/rest/DockerController.java
@@ -1,5 +1,7 @@
 package com.example.demo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
 
     @RequestMapping(value = "/docker/health",  method = RequestMethod.GET, produces = "text/plain")
-    public String getHealthStatus(){
-        return null;
+    public ResponseEntity getHealthStatus(){
+        return new ResponseEntity( "Unregistered version!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
