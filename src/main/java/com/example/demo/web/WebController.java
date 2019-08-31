0c1f53484e3a29ed89c7a3144a76756523e1ff0e
aradosz79 <artur@icole.eu>
2019-08-31 14:54:10 +0200

    Added register form

--- src/main/java/com/example/demo/web/WebController.java
+++ src/main/java/com/example/demo/web/WebController.java
@@ -1,12 +1,16 @@
 package com.example.demo.web;
 
 import com.example.demo.service.RegisterService;
+import com.example.demo.web.form.RegistrationForm;
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.concurrent.TimeUnit;
 
@@ -33,8 +37,15 @@ public class WebController {
     }
     @RequestMapping("/register")
     @Timed
-    public String register(){
+    public String register(Model model){
+        model.addAttribute("page","register");
+        return "default";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(Model model, @ModelAttribute RegistrationForm registrationForm){
         registerService.register();
+        model.addAttribute("registered","true");
         return "default";
     }
 }
